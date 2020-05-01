94ce2098f51a277e33ac728417092fb87ba715fb
MenosGrandes <menosg@tutanota.com>
2020-05-01 16:59:27 +0200

    Minor changes

--- NVIM/run_nvim.sh
+++ NVIM/run_nvim.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
-local MOUNT_PATH=$1
-local DOCKER_NAME=$2
-docker run -it  -v %MOUNT_PATH%:/code %DOCKER_NAME%
+#!/bin/bash
+MOUNT_PATH=$1
+DOCKER_NAME=$2
+docker run -it  -v $MOUNT_PATH:/code $DOCKER_NAME
