3de3ca3087a82c150db553bf4a925a5a6c91a254
MenosG <menosg@tutanota.com>
2020-11-18 17:35:43 +0100

    Added docker-compose for wireguard/airsonic/grocy. Updated phpVbox run.sh

--- phpVBox/run.sh
+++ phpVBox/run.sh
@@ -4,6 +4,6 @@ IMAGE_NAME=$2
 VBOX_MACHINE_IP=$3
 #  VBOX_MACHINE_IP must be an IP of machine that VBoxServer is running
 #to proper run the VBoxServer the IP must be set explicitly:
-# .\VBoxWebSrv.exe --host 192.168.0.165 
+# .\VBoxWebSrv.exe --host 192.168.0.165
 #Additionaly to the VBOX_MACHINE_IP must contain the port on which VBoxServer is running. Default port is 18083
-docker run --name $CONTAINER_NAME --restart=always -p 85:80 -e ID_HOSTPORT=$VBOX_MACHINE_IP  -e ID_NAME=localhost -d $IMAGE_NAME
+docker run --name $CONTAINER_NAME --restart=always -p 85:80 -e CONF_noAuth='true' -e ID_HOSTPORT=$VBOX_MACHINE_IP  -e ID_NAME=localhost -d $IMAGE_NAME
