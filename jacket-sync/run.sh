673f5de1541297236ceca1e98a1e07ee231ab39c
mg <mg@mg>
2024-07-03 17:26:39 +0100

    Added newest docker compose version

--- jacket-sync/run.sh
+++
@@ -1,1 +0,0 @@
-docker-compose  -f ../common/common-services.yml -f ../common/download_volume.yml -f production-compose.yml --env-file ".env" config > docker-compose.yaml
--- radarr/run.sh
+++ radarr/run.sh
@@ -1,2 +1,2 @@
 
-docker-compose  -f ../common/common-services.yml -f ../common/download_volume.yml -f production-compose.yml --env-file ".env" config > docker-compose.yaml
+docker compose  -f ../common/common-services.yml -f ../common/download_volume.yml -f production-compose.yml --env-file ".env" config > docker-compose.yaml
